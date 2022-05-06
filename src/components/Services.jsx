5562040a1771655925c92ce6015752f809edfead
ChrisMak <cmakride@gmail.com>
2022-05-06 16:57:08 -0400

    Style Size Services

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -22,18 +22,15 @@ const data = [
 
 const Services = () => {
   return (
-    <div className="grid grid-cols-1 gap-4 mx-5 justify-items-center md:grid-cols-2 lg:grid-cols-2">
+    <div className="grid grid-cols-1 gap-10 mx-5 justify-items-center md:grid-cols-1 lg:grid-cols-2">
 
       {data.map((service, idx) => (
 
-        <div className="h-96 w-96 max-w-xs border-2 border-gray-200 border-opacity-600 rounded-lg overflow-hidden bg-gradient-to-r from-light-100 to-dark-100" key={idx}>
+        <div className="sm:h-96 sm:w-11/12 max-w-3xl rounded-lg overflow-hidden" key={idx}>
           <Link to={service.link}>
-            <img src={service.image} alt="spirit" className="w-full h-80 object-cover object-center" />
+            <img src={service.image} alt="spirit" className="w-full h-full object-cover object-center" />
           </Link>
         </div>
-
-
-
       ))}
 
 
