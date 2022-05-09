465ae7518b5d0d7e646847ee29a2d9c2c45c3a64
ChrisMak <cmakride@gmail.com>
2022-05-09 16:39:58 -0400

    Pdf Viewing Web

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,10 +9,10 @@ const Footer = () => {
         <span className="text-sm  sm:text-center font-main tracking-wider text-sky-800">© 2022 Bruna Makrides.
         </span>
         <div className="flex mt-4 gap-4 space-x-6 sm:justify-center md:mt-0">
-            <a href="" className="text-sky-800 text-3xl hover:text-white">
+            <a href="" className="text-sky-800 text-3xl hover:text-white" rel='noreferrer' target='_blank'>
                 <BsWhatsapp/>
             </a>
-            <a href="https://www.instagram.com/brunamakrides/" className="text-sky-800 text-3xl hover:text-white">
+            <a href="https://www.instagram.com/brunamakrides/" className="text-sky-800 text-3xl hover:text-white" rel='noreferrer' target='_blank'>
                 <BsInstagram/>
             </a>
             
--- src/pages/Multihealing.jsx
+++ src/pages/Multihealing.jsx
@@ -1,10 +1,25 @@
-const Multihealing = () => {
-  return ( 
-    <>
-    <h2>MULTIHEALING</h2>
+import React from "react";
+import { Worker } from "@react-pdf-viewer/core";
+import { Viewer } from "@react-pdf-viewer/core";
+import '@react-pdf-viewer/core/lib/styles/index.css'
+
+import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
+import '@react-pdf-viewer/default-layout/lib/styles/index.css';
+
+import mdPDF from '../assets/md.pdf'
+
+
+
+export default function AllPages() {
+  const defaultLayoutPluginInstance = defaultLayoutPlugin();
+  
+return (
     
-    </>
-   );
+    <div>
+      <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.13.216/build/pdf.worker.min.js">
+        <Viewer fileUrl={mdPDF}
+        plugins={[defaultLayoutPluginInstance]}></Viewer>
+      </Worker>
+      </div>
+  );
 }
- 
-export default Multihealing;
