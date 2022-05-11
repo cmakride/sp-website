42e6bdf7235539bcc9a1e66945c87fd626762819
ChrisMak <cmakride@gmail.com>
2022-05-11 12:19:56 -0400

    About Style

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,25 +3,24 @@ import { MdDoubleArrow } from 'react-icons/md'
 
 const About = () => {
   return (
-    <div className='overflow-hidden mx-6 w-full py-16'>
-      
-        <div className="m-auto px-6">
-          <div className="space-y-6 lg:space-y-0 lg:flex lg:gap-12 lg:items-center">
-            <div className="lg:w-5/12">
-              <img className="rounded-lg shadow-lg" src={me} alt="" />
-            </div>
-            <div className="lg:w-6/12 flex flex-col items-center">
-              <h2 className="font-main text-4xl font-medium tracking-wider text-sky-800 ">Meet Bruna</h2>
-              <p className="mt-6 font-main text-2xl font-light tracking-wider">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum omnis voluptatem accusantium nemo perspiciatis delectus atque autem! Voluptatum tenetur beatae unde aperiam, repellat expedita consequatur! Officiis id consequatur atque doloremque!</p>
-              <p className="mt-6 font-main text-2xl font-light tracking-wider"> Nobis minus voluptatibus pariatur dignissimos libero quaerat iure expedita at? Asperiores nemo possimus nesciunt dicta veniam aspernatur quam mollitia.</p>
-              <p className="mt-6 font-main text-2xl font-light tracking-wider">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum omnis voluptatem accusantium nemo perspiciatis delectus atque autem! Voluptatum tenetur beatae unde aperiam, repellat expedita consequatur! Officiis id consequatur atque doloremque!</p>
-              <p className="mt-6 font-main text-2xl font-light tracking-wider"> Nobis minus voluptatibus pariatur dignissimos libero quaerat iure expedita at? Asperiores nemo possimus nesciunt dicta veniam aspernatur quam mollitia.</p>
-            </div>
-          </div>
-        </div>
-      
+    <div className='grid grid-cols-1 lg:grid-cols-2 justify-items-center overflow-hidden w-full gap-10 py-16 max-w-7xl'>
+
+      <img className="rounded-lg shadow-lg sm:w-10/12 max-w-xl" src={me} alt="" />
+
+      <div className="max-w-2xl mx-10 lg:mx-0 flex flex-col items-center">
+        <h2 className="font-main text-4xl font-medium tracking-wider text-sky-800 ">Meet Bruna</h2>
+        <p className="mt-6 font-main text-2xl font-light tracking-wider">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum omnis voluptatem accusantium nemo perspiciatis delectus atque autem! Voluptatum tenetur beatae unde aperiam, repellat expedita consequatur! Officiis id consequatur atque doloremque!</p>
+        <p className="mt-6 font-main text-2xl font-light tracking-wider"> Nobis minus voluptatibus pariatur dignissimos libero quaerat iure expedita at? Asperiores nemo possimus nesciunt dicta veniam aspernatur quam mollitia.</p>
+        <p className="mt-6 font-main text-2xl font-light tracking-wider">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum omnis voluptatem accusantium nemo perspiciatis delectus atque autem! Voluptatum tenetur beatae unde aperiam, repellat expedita consequatur! Officiis id consequatur atque doloremque!</p>
+        <p className="mt-6 font-main text-2xl font-light tracking-wider"> Nobis minus voluptatibus pariatur dignissimos libero quaerat iure expedita at? Asperiores nemo possimus nesciunt dicta veniam aspernatur quam mollitia.</p>
+      </div>
+
+
+
 
     </div>
+
+    
 
   );
 }
