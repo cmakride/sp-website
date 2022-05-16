5390e4adc1edb6c3612e3582ea713cc40500d102
ChrisMak <cmakride@gmail.com>
2022-05-16 11:39:17 -0400

    Service Text Responsive

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -11,7 +11,15 @@ const Services = () => {
       <div className="max-w-screen-2xl w-full mt-4 grid grid-cols-1 gap-10 justify-items-center lg:grid-cols-2">
 
         <Link className="flex bg-multi bg-cover bg-center h-96 w-11/12 max-w-3xl rounded-lg overflow-hidden shadow-lg mx-6" to="/multihealing">
-          <h2 className="self-end w-full pb-6 inset-x-0 text-white font-main text-3xl font-light text-center tracking-wider leading-4">Multi Dimensional Healing</h2>
+          <div className="flex justify-center gap-3 flex-wrap self-end w-full inset-x-0 text-white font-main text-3xl font-light tracking-wider leading-4 mb-6">
+            <p>
+              Multi Dimensional
+            </p>
+            <p>
+              Healing
+            </p>
+          </div>
+
         </Link>
 
         <Link className="flex bg-soulplan bg-cover bg-center h-96 w-11/12 max-w-3xl rounded-lg overflow-hidden shadow-lg mx-6" to="/soulplan">
