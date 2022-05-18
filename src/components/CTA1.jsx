a523068f2333c003c0593a955df35739d1278a13
ChrisMak <cmakride@gmail.com>
2022-05-17 23:28:46 -0400

    Text Update

--- src/components/CTA1.jsx
+++ src/components/CTA1.jsx
@@ -7,7 +7,7 @@ const CTA1 = () => {
         <p className="m-0  p-0  font-main text-lg lg:text-2xl font-light tracking-wider text-rosebrown-500 text-center">Não hesite em nos enviar uma mensagem no WhatsApp para mais informações e agendamento dos serviços!</p>
         <div className=" mt-3  flex  flex-row  justify-center">
 
-          <a href="https://wa.me/5581993365820/" className="p-3  w-64  bg-rosebrown-300 text-lg text-white hover:bg-white hover:text-rosebrown-300 text-center"rel='noreferrer' target='_blank'>Quero agendar minha a Sessão!</a>
+          <a href="https://wa.me/5581993365820/" className="p-3  w-64  bg-rosebrown-300 text-lg text-white hover:bg-white hover:text-rosebrown-300 text-center"rel='noreferrer' target='_blank'>Quero agendar minha Sessão!</a>
         </div>
       </div>
     </div>
