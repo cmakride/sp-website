56535fbab548acf9669cb79c7a1a6c5a7a87622d
ChrisMak <cmakride@gmail.com>
2022-05-17 20:47:18 -0400

    New Favicon

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -5,7 +5,7 @@ const Services = () => {
   return (
     <>
       <div className="flex content-center justify-center border-b w-1/2 p-4 font-main text-2xl font-medium tracking-wider text-rosebrown-300">
-        <p>SERVICES</p>
+        <p>SERVIÇOS</p>
       </div>
 
       <div className="max-w-screen-2xl w-full mt-4 grid grid-cols-1 gap-10 justify-items-center lg:grid-cols-2">
@@ -13,17 +13,26 @@ const Services = () => {
         <Link className="flex bg-multi bg-cover bg-center h-96 w-11/12 max-w-3xl rounded-lg overflow-hidden shadow-lg mx-6" to="/multihealing">
           <div className="flex justify-center gap-3 flex-wrap self-end w-full inset-x-0 text-white font-main text-3xl font-light tracking-wider leading-4 mb-6">
             <p>
-              Multi Dimensional
+              Terapia 
             </p>
             <p>
-              Healing
+            Multidimensional
             </p>
           </div>
 
         </Link>
 
         <Link className="flex bg-soulplan bg-cover bg-center h-96 w-11/12 max-w-3xl rounded-lg overflow-hidden shadow-lg mx-6" to="/soulplan">
-          <h2 className="self-end w-full pb-6 inset-x-0 text-white font-main text-3xl font-light text-center tracking-wider leading-4">Soul Plan</h2>
+        <div className="flex justify-center gap-3 flex-wrap self-end w-full inset-x-0 text-white font-main text-3xl font-light tracking-wider leading-4 mb-6">
+            <p>
+            Leitura do Plano de 
+            </p>
+            <p>
+            Alma
+          (Soul Plan®)
+            </p>
+          </div>
+          
         </Link>
 
       </div>
