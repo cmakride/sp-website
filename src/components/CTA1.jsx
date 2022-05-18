6dcb8eea6cea0e09c06cae9f070c835285c322ca
ChrisMak <cmakride@gmail.com>
2022-05-17 22:23:42 -0400

    Text update

--- src/components/CTA1.jsx
+++ src/components/CTA1.jsx
@@ -3,11 +3,11 @@ const CTA1 = () => {
 
     <div className="mt-10 w-full flex content-center justify-center bg-rosebrown-200">
       <div className="max-w-4xl w-7/10 p-10  py-20  flex  flex-col  flex-wrap  justify-center  content-center gap-3">
-        <div className="m-0  p-0  font-main text-xl lg:text-2xl font-medium tracking-wider text-rosebrown-500 text-center">Request Info or Book a Session</div>
-        <p className="m-0  p-0  font-main text-lg lg:text-2xl font-light tracking-wider text-rosebrown-500 text-center">To easily request additional material with detailed information on each of these services, get in touch with me!</p>
+        <div className="m-0  p-0  font-main text-xl lg:text-2xl font-medium tracking-wider text-rosebrown-500 text-center">Sentiu o chamado?</div>
+        <p className="m-0  p-0  font-main text-lg lg:text-2xl font-light tracking-wider text-rosebrown-500 text-center">Não hesite em nos enviar uma mensagem no what’s app para mais informações e agendamento dos serviços!</p>
         <div className=" mt-3  flex  flex-row  justify-center">
 
-          <a href="https://wa.me/5581993365820/" className="p-3  w-64  bg-rosebrown-300 text-lg text-white hover:bg-white hover:text-rosebrown-300 text-center"rel='noreferrer' target='_blank'>Book a Session</a>
+          <a href="https://wa.me/5581993365820/" className="p-3  w-64  bg-rosebrown-300 text-lg text-white hover:bg-white hover:text-rosebrown-300 text-center"rel='noreferrer' target='_blank'>Quero agendar minha sessão</a>
         </div>
       </div>
     </div>
